bb5da393b92f30ec66d149843f5ee709882727c0
LaurenSwartz <swa21010@byui.edu>
2025-02-06 19:35:27 +0200

    week 4 project complete

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,4 +1,5 @@
 const isAuthenticated= (req, res, next) => {
+    console.log(req.session)
     if (req.session.user ===undefined){
         return res.status(401).json("You do not have access.");
     }
